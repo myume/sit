7a5a82e0adbcd9bd8dca915ea84874b32e6c2945
yum <contact@meyume.com>
2026-03-15 19:59:45 -0400

    feat: setup stb_image.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
-#include <iostream>
-#include <ostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <print>
 
-int main() { std::cout << "Hello World" << std::endl; }
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::println(stderr, "Usage: sit <image_path>");
+        return -1;
+    }
+
+    int width, height, channels;
+    stbi_uc *pixels = stbi_load(argv[1], &width, &height, &channels, 0);
+
+    std::println("{} {}", width, height);
+}
